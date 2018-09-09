768f3c23df3eaf69d952c774c58dc66937d68a03
futamidaiki <nimi0370@gmail.com>
2018-09-09 22:53:36 +0900

    futame

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // https://www.foobar.com/my-app/
   // then change this to '/my-app/'
   baseUrl: process.env.NODE_ENV === 'production'
-    ? '..docs/'
+    ? '/futame/'
     : '/',
 
   // where to output built files
